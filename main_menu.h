e8af39373ad94cda41bdd5f5dabc5bf5c21fd86a
Stephen Aranda <aranda.stephen88@gmail.com>
2022-12-31 21:59:35 -0600

    declared function to handle user inp at menu

--- main_menu.h
+++ main_menu.h
@@ -10,11 +10,19 @@
 #ifndef _MAIN_MENU_
 #define _MAIN_MENU_
 
+
+// Print the main menu on screen.
 void printMainMenu(){
    printf_s("1. create account\n");
    printf_s("2. view account\n");
    printf_s("3. delete account\n");
    printf_s("4. exit bank app\n");
+}
+
+// handles the input for the main menu -- i.e user presses either 1,2,3, or 4 then handle
+// what happens next.
+void handleMainMenuInput(){
+    
 }
 
 
