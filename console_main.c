b5476eccee49193a9d3ba88abf7260c6275c7448
Stephen Aranda <aranda.stephen88@gmail.com>
2023-01-04 19:40:45 -0600

    created stubs

--- console_main.c
+++ console_main.c
@@ -6,7 +6,7 @@
   
 */
 
-#include "title.h"
+
 
 int main(){
 
---
+++ view_account.h
@@ -0,0 +1,6 @@
+/*
+* Author: Stephen Aranda
+* Date  : 1/4/2023
+* file  : view_account.h
+* Desc  : Module for the view account function to view account from user input.
+*/
