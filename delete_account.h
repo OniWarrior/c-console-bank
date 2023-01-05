b3de19bef227ce22fd9defa37b17b325416b75bb
Stephen Aranda <aranda.stephen88@gmail.com>
2023-01-04 19:43:32 -0600

    created stubs

---
+++ delete_account.h
@@ -0,0 +1,6 @@
+/*
+* Author: Stephen Aranda
+* Date  : 1/4/2023
+* File  : delete_account.h
+* Desc  : Module for the delete account function that deletes account from user input.
+*/
---
+++ say_goodbye.h
@@ -0,0 +1,6 @@
+/*
+ * Author: stephen aranda
+ * Date  : 1/4/2023
+ * File  : say_goodbye.h
+ * Desc  : Module for the say goodbye function to exit app.
+ */
